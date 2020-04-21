6f3c1c34d7b647c4ef7a03dec8ed78376197505d
ThatJohn <fajardocj@gmail.com>
2020-04-21 10:03:45 -0400

    Adds full CRUD  to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,6 +4,27 @@ class StoriesController < ApplicationController
         render json: @stories, :include => {:user => {:only => :name}, :comments => {:only => :comment}}
     end
 
+    def create
+        @story = Story.create(story_params)
+    end
+
+    def show
+        @story = Story.find(params[:id])
+        render json: @story, :include => {:user => {:only => :name}}
+    end
+
+    def update
+        @story = Story.find(params[:id])
+        @story.update_attributes(story_params)
+        render json: @story
+    end
+  
+    def destroy
+        @story = Story.find(params[:id])
+        @story.destroy
+        render json: @story
+    end
+
     private
   
     def story_params
