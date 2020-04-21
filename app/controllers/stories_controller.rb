d3be33ba566d14630801c98201859cce8f2e69dd
ThatJohn <fajardocj@gmail.com>
2020-04-21 10:02:35 -0400

    Adds index to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,6 @@
 class StoriesController < ApplicationController
+    def index
+        @stories = Story.all
+        render json: @stories, :include => {:user => {:only => :name}, :comments => {:only => :comment}}
+    end
 end
